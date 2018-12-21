22e9b03cdbe491401731aa677e304e9fc1cc4c5e
Nader Toukabri <nader@emiketic.com>
2018-12-21 22:22:49 +0100

    update polyfill

--- src/common/polyfill.test.helper.js
+++ src/common/polyfill.test.helper.js
@@ -1,17 +1,20 @@
-const areIntlLocalesSupported = require('intl-locales-supported');
+/**
+ * Intl
+ */
 
-const locales = ['fr-FR'];
+const LOCALES = ['en'];
 
 if (global.Intl) {
-  // Determine if the built-in `Intl` has the locale data we need.
-  if (!areIntlLocalesSupported(locales)) {
-    // `Intl` exists, but it doesn't have the data we need, so load the
-    // polyfill and patch the constructors we need with the polyfill's.
+  if (
+    !Intl.NumberFormat
+    || Intl.NumberFormat.supportedLocalesOf(LOCALES).length !== LOCALES.length
+    || !Intl.DateTimeFormat
+    || Intl.DateTimeFormat.supportedLocalesOf(LOCALES).length !== LOCALES.length
+  ) {
     const IntlPolyfill = require('intl');
     Intl.NumberFormat = IntlPolyfill.NumberFormat;
     Intl.DateTimeFormat = IntlPolyfill.DateTimeFormat;
   }
 } else {
-  // No `Intl`, so use and load the polyfill.
   global.Intl = require('intl');
 }
