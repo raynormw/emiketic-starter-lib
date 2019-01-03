bc40cf2bf483de97d1960de5900cb850728a9f16
Nader Toukabri <nader.toukabri@gmail.com>
2019-01-03 01:24:00 +0100

    fix build

--- dist/common/intl.js
+++ dist/common/intl.js
@@ -7,7 +7,7 @@ Object.defineProperty(exports, "__esModule", {
 var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
 exports.defineLocale = defineLocale;
-var TIME_ZONE = Intl.DateTimeFormat().resolvedOptions().timeZone;
+var TIME_ZONE = exports.TIME_ZONE = Intl.DateTimeFormat().resolvedOptions().timeZone;
 
 function defineLocale(locale) {
   var defaults = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
